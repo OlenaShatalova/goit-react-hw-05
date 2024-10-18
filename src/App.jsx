192bfca3c6429955fb94ee8900a3b1757b089ffd
Olena Shatalova <164022674+OlenaShatalova@users.noreply.github.com>
2024-10-18 22:54:30 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Comp1 from './components/Comp1/Comp1';
+import Comp2 from './components/Comp2/Comp2';
+
+function App() {
+  return (
+    <>
+      <Comp1 />
+      <Comp2 />
+    </>
+  );
+}
+
+export default App;
